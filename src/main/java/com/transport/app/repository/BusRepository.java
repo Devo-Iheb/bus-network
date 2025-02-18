ac824c155772a166fe08c109471f9aef3a2c6964
Devo-Iheb <iheb.ben.chaabane@devoteam.com>
2025-02-18 14:46:24 +0100

    Résolution des conflits

---
+++ src/main/java/com/transport/app/repository/BusRepository.java
@@ -0,0 +1,9 @@
+package com.transport.app.repository;
+
+import com.transport.app.model.Bus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BusRepository extends JpaRepository<Bus, Long> {
+}
