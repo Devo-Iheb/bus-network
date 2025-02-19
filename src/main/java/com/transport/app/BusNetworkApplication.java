fd5a3b6368764f46ad3d86627ad2d37ceec725fd
Devo-Iheb <iheb.ben.chaabane@devoteam.com>
2025-02-19 11:30:22 +0100

    ajout methode websocket

--- src/main/java/com/transport/app/BusNetworkApplication.java
+++ src/main/java/com/transport/app/BusNetworkApplication.java
@@ -2,10 +2,29 @@ package com.transport.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+import com.transport.app.websocket.BusWebSocketHandler;
 
 @SpringBootApplication
-public class BusNetworkApplication {
+@EnableWebSocket
+public class BusNetworkApplication implements WebSocketConfigurer {
+
     public static void main(String[] args) {
         SpringApplication.run(BusNetworkApplication.class, args);
+    }
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(busWebSocketHandler(), "/bus-tracker").setAllowedOrigins("*");
+    }
+
+    // ✅ Ajoute cette méthode pour créer l'instance de BusWebSocketHandler
+    @Bean
+    public BusWebSocketHandler busWebSocketHandler() {
+        return new BusWebSocketHandler();
     }
 }
