c90cd3ba0ca68f771b29729b287bbbd12bc3db16
Devo-Iheb <iheb.ben.chaabane@devoteam.com>
2025-02-19 11:00:19 +0100

    fix classe  websocket

--- src/main/java/com/transport/app/websocket/BusWebSocketHandler.java
+++ src/main/java/com/transport/app/websocket/BusWebSocketHandler.java
@@ -1,24 +1,13 @@
 package com.transport.app.websocket;
 
-import org.springframework.web.socket.handler.TextWebSocketHandler;
-import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.TextMessage;
-
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
-
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
 import java.io.IOException;
 import java.util.concurrent.CopyOnWriteArrayList;
 
-
-
-public class BusWebSocketHandler extends TextWebSocketHandler {
+public class BusWebSocketHandler extends TextWebSocketHandler { // Hérite bien de TextWebSocketHandler
     private static final CopyOnWriteArrayList<WebSocketSession> sessions = new CopyOnWriteArrayList<>();
-
-    @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(busWebSocketHandler(), "/bus-tracker").setAllowedOrigins("*");
-}
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) {
